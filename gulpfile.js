0321a26e3ccb7e74446a226bc2f794df301002e1
Allan Gonçalves da Cruz <allangcruz@gmail.com>
2016-01-25 16:40:54 -0200

    package jason e grulpfile ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
-var gulp = require('gulp');
+var gulp      = require('gulp'),
+    prefix    = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    refresh   = require('gulp-livereload'),
+    server    = require('tiny-lr')();
+
+var src = "assets/**/*";
+
+gulp.task('watch', function() {
+    server.listen(3000, function( err ) {
+        if (err) { return console.log( err ); }
  
+        gulp.watch(src, [
+            'compileStyles'
+        ]);
+    });
+});
+
+
+
 gulp.task('default', function() {
     // Tarefas
 });
