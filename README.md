bb0f5c8651c97aa5300f9a80dfc5d50b4a0c87e8
Allan Gonçalves da Cruz <allangcruz@gmail.com>
2016-01-27 21:33:43 -0200

    Templates finalizado e enviando emails

--- README.md
+++ README.md
@@ -2,8 +2,3 @@
 
 Implementação de layout em psd
 
-# Pacote de envio de email
-
-```
-https://packagist.org/packages/werx/email
-```
--- backend/EnviarEmail.php
+++ backend/EnviarEmail.php
@@ -1,5 +1,45 @@
 <?php 
+header("Content-type: text/html; charset=utf-8");
 
-require_once "../vendor/autoload.php";
+require_once('phpmailer/class.phpmailer.php');
+require_once('phpmailer/PHPMailerAutoload.php');
 
-use werx\Email; 
+//contem a mensagem formatada
+require_once "mensagemEmail.php";
+
+$nome = isset($_POST['nome']) ? $_POST['nome']:'';
+$email = isset($_POST['email']) ? $_POST['email']:'';
+$telefone = isset($_POST['telefone']) ? $_POST['telefone']:'';
+
+$mail = new PHPMailer;
+$mail->isSMTP();                                      
+$mail->CharSet = "utf-8"; 
+$mail->Port = 25;                                    
+
+$mail->Host = 'mail.allangcruz.com.br';  
+$mail->SMTPAuth = true;                             
+$mail->Username = 'teste@allangcruz.com.br';              
+$mail->Password = 'teste123';    
+
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+
+$mail->setFrom('teste@allangcruz.com.br', 'Allan Gonçalves da Cruz');
+$mail->addAddress($email, $nome);
+$mail->addBCC('teste@allangcruz.com.br');
+$mail->isHTML(true);
+$mail->Subject = 'eBook -  Gestão de Representantes Comerciais';
+
+$mail->Body    = $mensagem;
+
+if($mail->send()) {
+	echo '<script>alert("Obrigado, verifique seu e-mail!");location.href="../";</script>';
+} else {
+	echo '<script>alert("Ocorreu um erro ao enviar o eBook ao seu e-mail, verifique se o mesmo esta correto!");</script>';
+	echo $mail->ErrorInfo;
+}
